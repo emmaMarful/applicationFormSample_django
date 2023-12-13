2ba91eb7ea5f1a15ea63993cef9b3ccb07293ea5
emmaMarful <eamarful@gmail.com>
2023-12-13 14:51:16 +0000

    sending the applicants and the host an email after submission

--- job_application/views.py
+++ job_application/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from forms import ApplicationForm
 from .models import Form
 from django.contrib import messages
+from django.core.mail import EmailMessage
+import backend as credential
 
 
 def index(request):
@@ -14,8 +16,20 @@ def index(request):
             date = form.cleaned_data["date"]
             occupation = form.cleaned_data["occupation"]
 
-            Form.objects.create(first_name=first_name, last_name=last_name, email=email, date=date,
+            Form.objects.create(first_name=first_name.title(), last_name=last_name.title(), email=email, date=date,
                                 occupation=occupation)
+
+            # email to applicant
+            message_body = f"A new job application was submitted. Thank you, {first_name.title()}"
+            email_message = EmailMessage("Form submission conformation", message_body, to=[email])
+            email_message.send()
+
+            # email to host
+            mes_body = f"{first_name} submitted an application with the details: \n" \
+                       f"First Name:{first_name.title()}  \n Last Name:{last_name.title()} \n" \
+                       f"Email:{email} \n Date:{date} \n Occupation:{occupation}"
+            host_email = EmailMessage("Form Submission", mes_body, to=[credential.username()])
+            host_email.send()
 
             messages.success(request, "Form submitted successfully!")
     return render(request, "index.html")
--- mysite/settings.py
+++ mysite/settings.py
@@ -122,3 +122,13 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+
+import backend as credentials
+EMAIL_HOST_USER = credentials.username()
+EMAIL_HOST_PASSWORD = credentials.password()
