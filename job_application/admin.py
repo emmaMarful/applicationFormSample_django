c0b478207faba7af5d65e5e401c40a6183c1f936
emmaMarful <eamarful@gmail.com>
2023-12-15 14:30:33 +0000

    assined a superuser

--- job_application/admin.py
+++ job_application/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Form
+
+
+class FormAdmin(admin.ModelAdmin):
+    list_display = ("first_name", "last_name", "email")
+    search_fields = ("first_name", "last_name", "email")
+    list_filter = ("date", "occupation")
+    readonly_fields = ("occupation",)
+
+
+admin.site.register(Form, FormAdmin)
 
 # Register your models here.
